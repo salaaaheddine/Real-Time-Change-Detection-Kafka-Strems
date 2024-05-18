f3d9b436b849733b29b515df51497b08a10d4895
Salah <theonegreat0@gmail.com>
2024-05-18 21:54:30 +0100

    Youtube Data Api Works

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import sys
 import logging
+import requests
+from config_file import config
+
+
 
 def main():
     logging.info("START")
+    google_api_key = config['google_api_key']
+    youtube_playlist_id = config['youtube_playlist_id']
+
+    response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", params={
+        "key": google_api_key,
+        "playlistId": youtube_playlist_id
+    })
+
+    logging.debug("GOT %s", response.text)
 
 
 if __name__ == '__main__':
